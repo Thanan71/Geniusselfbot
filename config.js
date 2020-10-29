8dd2794a0e9f5ebc5efbfeb1404e84292041d9ee
Thanan71 <38879178+Thanan71@users.noreply.github.com>
2020-10-29 11:56:08 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Vous devez Remplir les champs ci-dessous en laissant les guillemets
+*/
+
+       const token = "NzA3MTYyODQ1MjQyMTk1OTc5.X5gWSA.GL9gV9D6U8BmxzvhnEcQTLcRyEM"; ///tu met ton token ici
+       const prefix = "/"  ///tu met ton prefix ici 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {//NE PAS TOUCHER
+    token,        //NE PAS TOUCHER
+    prefix        //NE PAS TOUCHER
+}                 //NE PAS TOUCHER
---
+++ start.bat
@@ -0,0 +1,2 @@
+node GeniusSelf.js
+pause
