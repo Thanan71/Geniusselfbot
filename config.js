dec4f2edbf83b500e5706ad809796598db1008e0
Thanan71 <38879178+Thanan71@users.noreply.github.com>
2020-10-29 12:02:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 Vous devez Remplir les champs ci-dessous en laissant les guillemets
 */
 
-       const token = "NzA3MTYyODQ1MjQyMTk1OTc5.X5gWSA.GL9gV9D6U8BmxzvhnEcQTLcRyEM"; ///tu met ton token ici
+       const token = "tontoken"; ///tu met ton token ici
        const prefix = "/"  ///tu met ton prefix ici 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
